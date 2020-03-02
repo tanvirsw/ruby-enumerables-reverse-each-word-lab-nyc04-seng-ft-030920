4f55643fef711fa61e470d690fd31ca24914edc5
Tanvir Walia <tarywalia@gmail.com>
2020-03-02 20:56:36 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
+© 2020 GitHub, Inc.
